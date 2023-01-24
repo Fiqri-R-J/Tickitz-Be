852efc3565c45070e5ebacb406233bb72d067648
master <fachri.fajar@gmail.com>
2023-01-24 22:29:15 +0700

    [add] schedules

--- source/controller/schedules.js
+++ source/controller/schedules.js
@@ -14,60 +14,356 @@ const addSchedules = async (req, res) => {
       cinema,
       available_seat,
     } = req.body;
-    const getMoviesName = await models.getMoviesName({ movie_name });
-    const getHour = duration.split(":")[0];
-    const getMins = duration.split(":")[1];
 
-    if (users_id !== "1") {
+    const roleValidator = req.users_id || null; // middleware for roleValidator
+    const getRole = await models.getRoles({ roleValidator });
+    const isAdmin = getRole[0]?.role;
+
+    if (isAdmin == "ADMIN" && roleValidator == "1") {
+      await models.addSchedules({
+        users_id: roleValidator,
+        movies_id,
+        time,
+        location,
+        price,
+        date_start,
+        date_end,
+        cinema,
+        available_seat,
+      });
+
+      res.status(201).json({
+        status: "true",
+        code: 201,
+        message: "Success add new Schedules",
+        data: { ...req.body },
+      });
+    } else {
       throw {
         code: 401,
-        message: "Access not granted!, only Admin can access this section",
+        message: "Access not granted, only admin can access this section!",
       };
     }
-    if (getMoviesName.length !== 0) {
-      throw {
-        code: 409,
-        message: "Movies name already exists",
-      };
-    }
-
-    let file = req.files.movie_picture;
-
-    cloudinary.v2.uploader.upload(
-      file.tempFilePath,
-      { public_id: uuidv4(), folder: "tickitz" },
-      async function (error, result) {
-        if (error) {
-          throw error;
-        }
-
-        await models.addMovies({
-          users_id,
-          movie_name,
-          category,
-          director,
-          casts,
-          release_date,
-          synopsis,
-          movie_picture: result.public_id,
-          duration,
-          duration_hour: getHour,
-          duration_mins: getMins,
-        });
-      }
-    );
-
-    res.status(201).json({
-      status: "true",
-      code: 201,
-      message: "Success add new movie",
-      data: { ...req.body },
-    });
   } catch (error) {
-    res.status(error?.code ?? 500).json({
-      message: error,
+    res.status(error.code || 500).json({
+      message: error.message,
     });
   }
 };
 
-module.exports = { addSchedules };
+const getSchedulesbyLocation = async (req, res) => {
+  try {
+    const { title } = req.params;
+    const { page, limit, sort } = req.query;
+
+    const totalDatas = await models.getAllSchedules();
+
+    let getSchedulesData;
+    let getAllData;
+
+    if (title) {
+      getSchedulesData = await models.getSchedulesByTitle({ title });
+      connectRedis.set("find_schedules", true, "ex", 10);
+      connectRedis.set("url", req.originalUrl, "ex", 10);
+      connectRedis.set("location", title, "ex", 10);
+      connectRedis.set(
+        "getReqSchedules",
+        JSON.stringify(getSchedulesData),
+        "ex",
+        10
+      );
+      if (getSchedulesData && getSchedulesData.length > 0) {
+        res.json({
+          message: `Get Schedules With Location: ${title}`,
+          code: 200,
+          data: getSchedulesData,
+        });
+      } else {
+        throw { code: 422, message: "Data not found" };
+      }
+    }
+    if (!title && !page && !limit && !sort) {
+      getSchedulesData = totalDatas;
+      connectRedis.set("url", req.originalUrl, "ex", 10);
+      connectRedis.set("find_all_schedules", true, "ex", 10);
+      connectRedis.set(
+        "getReqSchedules",
+        JSON.stringify(getSchedulesData),
+        "ex",
+        10
+      );
+      res.json({
+        message: "Success get all data schedules",
+        code: 200,
+        total: getSchedulesData.length,
+        data: getSchedulesData,
+      });
+    }
+    if (page || limit || sort) {
+      if (page && limit && sort) {
+        getAllData = await models.getAllSchedulesPaginationSort({
+          sort,
+          limit,
+          page,
+        });
+      } else if (page && limit) {
+        getAllData = await models.getAllSchedulesPagination({ limit, page });
+        connectRedis.set("url", req.originalUrl, "ex", 10);
+        connectRedis.set("page", page, "ex", 10);
+        connectRedis.set("limit", limit, "ex", 10);
+        connectRedis.set("dataPerPage", JSON.stringify(getAllData), "ex", 10);
+        connectRedis.set("getReqAccPagi", JSON.stringify(totalDatas), "ex", 10);
+        connectRedis.set("isPaginated", true, "ex", 10);
+      } else if (sort) {
+        getAllData = await models.getAllSchedulesSort({ sort });
+        connectRedis.set("url", req.originalUrl, "ex", 10);
+        connectRedis.set("isSorted", true, "ex", 10);
+        connectRedis.set("sortedData", JSON.stringify(getAllData), "ex", 10);
+        res.json({
+          message: "Success get all schedules",
+          total: getAllData.length,
+          data: getAllData,
+        });
+      }
+    }
+
+    if ((page && limit && sort) || (page && limit)) {
+      connectRedis.set("url", req.originalUrl, "ex", 10);
+      connectRedis.set("page", page, "ex", 10);
+      connectRedis.set("limit", limit, "ex", 10);
+      connectRedis.set("dataPerPage", JSON.stringify(getAllData), "ex", 10);
+      connectRedis.set("getReqAccPagi", JSON.stringify(totalDatas), "ex", 10);
+      connectRedis.set("isPaginated", true, "ex", 10);
+      res.json({
+        message: "success get schedules",
+        code: 200,
+        total: totalDatas.length,
+        dataPerPage: getAllData.length,
+        page: `${page} from ${Math.ceil(totalDatas.length / limit)}`,
+        data: getAllData,
+      });
+    }
+  } catch (error) {
+    res.status(error?.code ?? 500).json({
+      serverMessage: error,
+      data: [],
+    });
+  }
+};
+
+const getSchedulesbyLocation2 = async (req, res) => {
+  try {
+    const { title } = req.params;
+    const { page, limit, sort } = req.query;
+
+    const totalDatas = await models.getAllSchedules2();
+
+    let getSchedulesData;
+    let getAllData;
+
+    if (title) {
+      getSchedulesData = await models.getSchedulesByTitle2({ title });
+      connectRedis.set("find_schedules", true, "ex", 10);
+      connectRedis.set("url", req.originalUrl, "ex", 10);
+      connectRedis.set("location", title, "ex", 10);
+      connectRedis.set(
+        "getReqSchedules",
+        JSON.stringify(getSchedulesData),
+        "ex",
+        10
+      );
+      if (getSchedulesData && getSchedulesData.length > 0) {
+        res.json({
+          message: `Get Schedules With Location: ${title}`,
+          code: 200,
+          data: getSchedulesData,
+        });
+      } else {
+        throw { code: 422, message: "Data not found" };
+      }
+    }
+    if (!title && !page && !limit && !sort) {
+      getSchedulesData = totalDatas;
+      connectRedis.set("url", req.originalUrl, "ex", 10);
+      connectRedis.set("find_all_schedules", true, "ex", 10);
+      connectRedis.set(
+        "getReqSchedules",
+        JSON.stringify(getSchedulesData),
+        "ex",
+        10
+      );
+      res.json({
+        message: "Success get all data schedules",
+        code: 200,
+        total: getSchedulesData.length,
+        data: getSchedulesData,
+      });
+    }
+    if (page || limit || sort) {
+      if (page && limit && sort) {
+        getAllData = await models.getAllSchedulesPaginationSort2({
+          sort,
+          limit,
+          page,
+        });
+      } else if (page && limit) {
+        getAllData = await models.getAllSchedulesPagination2({ limit, page });
+        connectRedis.set("url", req.originalUrl, "ex", 10);
+        connectRedis.set("page", page, "ex", 10);
+        connectRedis.set("limit", limit, "ex", 10);
+        connectRedis.set("dataPerPage", JSON.stringify(getAllData), "ex", 10);
+        connectRedis.set("getReqAccPagi", JSON.stringify(totalDatas), "ex", 10);
+        connectRedis.set("isPaginated", true, "ex", 10);
+      } else if (sort) {
+        getAllData = await models.getAllSchedulesSort2({ sort });
+        connectRedis.set("url", req.originalUrl, "ex", 10);
+        connectRedis.set("isSorted", true, "ex", 10);
+        connectRedis.set("sortedData", JSON.stringify(getAllData), "ex", 10);
+        res.json({
+          message: "Success get all schedules",
+          total: getAllData.length,
+          data: getAllData,
+        });
+      }
+    }
+
+    if ((page && limit && sort) || (page && limit)) {
+      connectRedis.set("url", req.originalUrl, "ex", 10);
+      connectRedis.set("page", page, "ex", 10);
+      connectRedis.set("limit", limit, "ex", 10);
+      connectRedis.set("dataPerPage", JSON.stringify(getAllData), "ex", 10);
+      connectRedis.set("getReqAccPagi", JSON.stringify(totalDatas), "ex", 10);
+      connectRedis.set("isPaginated", true, "ex", 10);
+      res.json({
+        message: "success get schedules",
+        code: 200,
+        total: totalDatas.length,
+        dataPerPage: getAllData.length,
+        page: `${page} from ${Math.ceil(totalDatas.length / limit)}`,
+        data: getAllData,
+      });
+    }
+  } catch (error) {
+    res.status(error?.code ?? 500).json({
+      serverMessage: error,
+      data: [],
+    });
+  }
+};
+
+const updateSchedulesPartial = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      users_id,
+      movies_id,
+      time,
+      location,
+      price,
+      date_start,
+      date_end,
+      cinema,
+      available_seat,
+    } = req.body;
+
+    const roleValidator = req.users_id || null; // middleware for roleValidator
+    const getRole = await models.getRoles({ roleValidator });
+    const isAdmin = getRole[0]?.role;
+
+    if (isAdmin == "ADMIN" && roleValidator == "1") {
+      const getAllData = await models.getSchedulesByID({ id });
+
+      if (getAllData.length == 0) {
+        throw { code: 400, message: "movies_id not identified" };
+      } else {
+        await models.updateSchedulesPartial({
+          defaultValue: getAllData[0],
+          users_id: roleValidator,
+          movies_id,
+          time,
+          location,
+          price,
+          date_start,
+          date_end,
+          cinema,
+          available_seat,
+          id,
+        });
+
+        res.json({
+          status: "true",
+          message: "data updated",
+          data: {
+            id,
+            ...req.body,
+          },
+        });
+      }
+    } else {
+      throw {
+        code: 401,
+        message: "Access not granted, only admin can access this section!",
+      };
+    }
+  } catch (error) {
+    console.log(error);
+    if (error.code !== 500) {
+      if (
+        error.message ==
+        'duplicate key value violates unique constraint "movies_movie_name_key"'
+      ) {
+        res.status(422).json({
+          message: "Movie with the provided movie_name already exists",
+        });
+      } else {
+        res.status(error.code || 500).json({
+          message: error.message ?? error,
+        });
+      }
+    } else {
+      res.status(error.code || 500).json({
+        message: error.message,
+      });
+    }
+  }
+};
+
+const deleteSchedules = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const getAllData = await models.getSchedulesByID({ id });
+
+    const roleValidator = req.users_id || null; // middleware for roleValidator
+    const getRole = await models.getRoles({ roleValidator });
+    const isAdmin = getRole[0]?.role;
+
+    if (isAdmin == "ADMIN" && roleValidator == "1") {
+      if (getAllData.length == 0) {
+        throw { code: 400, message: "movies_id not identified" };
+      }
+
+      await models.deleteSchedules({ id });
+      res.json({
+        status: "true",
+        message: "MOVIE DELETED!",
+      });
+    } else {
+      throw {
+        code: 401,
+        message: "Access not granted, only admin can access this section!",
+      };
+    }
+  } catch (error) {
+    res.status(error?.code ?? 500).json({
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  addSchedules,
+  getSchedulesbyLocation,
+  getSchedulesbyLocation2,
+  updateSchedulesPartial,
+  deleteSchedules,
+};
--- source/middleware/schedules.js
+++ source/middleware/schedules.js
@@ -1,131 +1,144 @@
 const {
-    Validator,
-    addCustomMessages,
-    extend,
-  } = require("node-input-validator");
-  
-  const createMoviesValidator = (req, res, next) => {
-    extend("regexDuration", () => {
-      if (/^([0-9]+:)+[0-9]+$/.test(req.body.duration)) {
-        return true;
-      } else {
-        return false;
-      }
-    });
-  
-    extend("regexReleaseDate", () => {
-      if (
-        /^(19|20)\d\d[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])$/.test(
-          req.body.release_date
-        )
-      ) {
-        return true;
-      } else {
-        return false;
-      }
-    });
-  
-    addCustomMessages({
-      "duration.regexDuration": `Delimiter between hours to minutes, must be a colon (:)`,
-      "movie_picture.required": `Missing files`,
-      "release_date.regexReleaseDate": `Using ISO format (yyyy-mm-dd) is a mandatory`,
-    });
-  
-    const rules = new Validator(req.body, {
-      users_id: "required",
-      movie_name: "required|minLength:1|maxLength:50",
-      category: "required|minLength:1|maxLength:100",
-      director: "required|minLength:1|maxLength:100",
-      casts: "required|minLength:1|maxLength:100",
-      release_date: "required|regexReleaseDate",
-      synopsis: "required",
-      movie_picture: req.body.movie_picture == "" ? "required|url" : "url",
-      duration: "required|regexDuration",
-    });
-  
-    rules.check().then((matched) => {
-      if (matched) {
-        next();
-      } else {
-        res.status(422).json({
-          message: rules.errors,
-        });
-      }
-    });
-  };
-  
-  const updateMoviesPartialValidator = (req, res, next) => {
-    const {
-      movie_name,
-      category,
-      director,
-      casts,
-      release_date,
-      synopsis,
-      movie_picture,
-      duration,
-    } = req.body;
-  
-    extend("regexDuration", () => {
-      if (/^([0-9]+:)+[0-9]+$/.test(req.body.duration)) {
-        return true;
-      } else {
-        return false;
-      }
-    });
-  
-    extend("regexReleaseDate", () => {
-      if (
-        /^(19|20)\d\d[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])$/.test(
-          req.body.release_date
-        )
-      ) {
-        return true;
-      } else {
-        return false;
-      }
-    });
-  
-    addCustomMessages({
-      "duration.regexDuration": `Delimiter between hours to minutes, must be a colon (:)`,
-      "movie_picture.required": `Missing files`,
-      "release_date.regexReleaseDate": `Using ISO format (yyyy-mm-dd) is a mandatory`,
-    });
-  
-    const rules = new Validator(req.body, {
-      movie_name: movie_name
-        ? "required|minLength:1|maxLength:50"
-        : "minLength:1|maxLength:50",
-      category: category
-        ? "required|minLength:1|maxLength:100"
-        : "minLength:1|maxLength:100",
-      director: director
-        ? "required|minLength:1|maxLength:100"
-        : "minLength:1|maxLength:100",
-      casts: casts
-        ? "required|minLength:1|maxLength:100"
-        : "minLength:1|maxLength:100",
-      release_date: release_date
-        ? "required|regexReleaseDate"
-        : "regexReleaseDate",
-      synopsis: synopsis ? "required" : "minLength:1",
-      movie_picture: req.body.movie_picture == "" ? "required|url" : "url",
-      duration: duration ? "required|regexDuration" : "regexDuration",
-    });
-  
-    rules.check().then((matched) => {
-      if (matched) {
-        next();
-      } else {
-        res.status(422).json({
-          message: rules.errors,
-        });
-      }
-    });
-  };
-  
-  module.exports = {
-    createMoviesValidator,
-    updateMoviesPartialValidator,
-  };
-  
+  Validator,
+  addCustomMessages,
+  extend,
+} = require("node-input-validator");
+
+const createSchedulesValidator = (req, res, next) => {
+  extend("regexTime", () => {
+    if (/^([0-9]+:)+[0-9]+$/.test(req.body.time)) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  extend("regexDateStart", () => {
+    if (
+      /^(19|20)\d\d[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])$/.test(
+        req.body.date_start
+      )
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  extend("regexDateEnd", () => {
+    if (
+      /^(19|20)\d\d[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])$/.test(
+        req.body.date_end
+      )
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  addCustomMessages({
+    "time.regexTime": `Delimiter between hours to minutes, must be a colon (:)`,
+    "date_start.regexDateStart": `Using ISO format (yyyy-mm-dd) is a mandatory`,
+    "date_end.regexDateEnd": `Using ISO format (yyyy-mm-dd) is a mandatory`,
+  });
+
+  const rules = new Validator(req.body, {
+    movies_id: "required",
+    time: "required|regexTime",
+    location: "required",
+    price: "required",
+    date_start: "required|regexDateStart",
+    date_end: "required|regexDateEnd",
+    cinema: "required",
+    available_seat: "required",
+  });
+
+  rules.check().then((matched) => {
+    if (matched) {
+      next();
+    } else {
+      res.status(422).json({
+        message: rules.errors,
+      });
+    }
+  });
+};
+
+const updateSchedulesPartialValidator = (req, res, next) => {
+  const {
+    users_id,
+    movies_id,
+    time,
+    location,
+    price,
+    date_start,
+    date_end,
+    cinema,
+    available_seat,
+  } = req.body;
+
+  extend("regexTime", () => {
+    if (/^([0-9]+:)+[0-9]+$/.test(req.body.time)) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  extend("regexDateStart", () => {
+    if (
+      /^(19|20)\d\d[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])$/.test(
+        req.body.date_start
+      )
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  extend("regexDateEnd", () => {
+    if (
+      /^(19|20)\d\d[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])$/.test(
+        req.body.date_end
+      )
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  addCustomMessages({
+    "time.regexTime": `Delimiter between hours to minutes, must be a colon (:)`,
+    "date_start.regexDateStart": `Using ISO format (yyyy-mm-dd) is a mandatory`,
+    "date_end.regexDateEnd": `Using ISO format (yyyy-mm-dd) is a mandatory`,
+  });
+
+  const rules = new Validator(req.body, {
+    movies_id: movies_id ? "required" : "minLength:1",
+    time: time ? "required|regexTime" : "regexTime",
+    location: location ? "required" : "minLength:1",
+    price: price ? "required" : "minLength:1",
+    date_start: date_start ? "required|regexDateStart" : "regexDateStart",
+    date_end: date_end ? "required|regexDateEnd" : "regexDateEnd",
+    cinema: cinema ? "required" : "minLength:1",
+    available_seat: available_seat ? "required" : "minLength:1",
+  });
+
+  rules.check().then((matched) => {
+    if (matched) {
+      next();
+    } else {
+      res.status(422).json({
+        message: rules.errors,
+      });
+    }
+  });
+};
+
+module.exports = {
+  createSchedulesValidator,
+  updateSchedulesPartialValidator,
+};
